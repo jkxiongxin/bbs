ef0abddfbc4088fd906b31b00501ce63c867cac5
hzxxing <hzxxing@tairanchina.com>
2018-03-03 23:18:02 +0800

    配置修改

--- src/main/java/com/example/demo/jpa/model/Comment.java
+++ src/main/java/com/example/demo/jpa/model/Comment.java
@@ -17,7 +17,7 @@ import java.sql.Timestamp;
 public class Comment {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String content;
--- src/main/java/com/example/demo/jpa/model/Posting.java
+++ src/main/java/com/example/demo/jpa/model/Posting.java
@@ -14,7 +14,7 @@ import java.sql.Timestamp;
 public class Posting {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private Timestamp createTime;
